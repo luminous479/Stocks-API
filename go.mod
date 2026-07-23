c5602e898a5dd060a03b11cf8b497dfd179d47b0
luminous479 <aklemasultanakhi@gmail.com>
2026-07-23 21:26:09 +0600

    feat: add CRUD routes for stock API

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/luminous479/Stocks-API
 
 go 1.26.3
+
+require github.com/gorilla/mux v1.8.1
--- router/routers.go
+++ router/routers.go
@@ -1,1 +1,19 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/luminous479/Stocks-API/middleware"
+)
+
+
+func Routers() {		
+  
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/stocks/{id}", middleware.GetStocks).Methods("GET")	
+	router.HandleFunc("/api/stocks", middleware.GetAllStocks).Methods("GET")
+	router.HandleFunc("/api/stocks", middleware.CreateStock).Methods("POST")
+	router.HandleFunc("/api/stocks/{id}", middleware.UpdateStock).Methods("PUT")
+	router.HandleFunc("/api/stocks/{id}", middleware.DeleteStock).Methods("DELETE")
+
+}
