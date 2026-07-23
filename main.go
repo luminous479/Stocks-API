5fe526d143c31f9ff9f2213ab77bcc1630a30b8d
luminous479 <aklemasultanakhi@gmail.com>
2026-07-23 21:33:05 +0600

    feat: initialize HTTP server

--- main.go
+++ main.go
@@ -1,2 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"github.com/luminous479/Stocks-API/router"
+)
+
+func main() {
+	r := router.Routers()
+	fmt.Println("Server is running on port 8080")
+	if err := r.ListenAndServe(":8080"); err != nil {
+		fmt.Println("Error starting server:", err)
+	}	
+
+
+}
